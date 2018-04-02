72d7b8f60007f58ebe945c68e71e11ac11f9683f
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-04-02 20:56:42 +0100

    Client: mock tests for listen

--- test/unit/Bridge/Client/ListenTest.php
+++ test/unit/Bridge/Client/ListenTest.php
@@ -9,24 +9,30 @@ use BitWasp\Trezor\Bridge\Client;
 use BitWasp\Trezor\Bridge\Exception\InvalidMessageException;
 use BitWasp\Trezor\Bridge\Exception\SchemaValidationException;
 use BitWasp\Trezor\Bridge\Http\HttpClient;
+use BitWasp\Trezor\Bridge\Message\Device;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Middleware;
 use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\RequestInterface;
 
-class BridgeVersionTest extends TestCase
+class ListenTest extends TestCase
 {
     private $contentTypeJson = 'application/json';
 
-    public function testMockBridgeVersion()
+    public function testMockListen()
     {
-        $body = [
-            'version' => '1.0.0'
-        ];
+        $deviceReq = new \stdClass();
+        $deviceReq->path = "hidabc123";
+        $deviceReq->session = null;
+        $deviceReq->product = "21324";
+        $deviceReq->vendor = "1";
+
+        $deviceRes = clone ($deviceReq);
+        $deviceRes->session = '2';
 
         $requests = [
-            new Response(200, ['Content-Type' => $this->contentTypeJson], \json_encode($body)),
+            new Response(200, ['Content-Type' => $this->contentTypeJson], \json_encode([$deviceRes])),
         ];
 
         // Create a mock and queue two responses.
@@ -42,9 +48,14 @@ class BridgeVersionTest extends TestCase
 
         $httpClient = HttpClient::forUri("http://localhost:21325/", ['handler' => $stack,]);
         $client = new Client($httpClient);
-        $response = $client->bridgeVersion();
 
-        $this->assertEquals('1.0.0', $response->version);
+        $device = new Device($deviceReq);
+        $response = $client->listen($device);
+
+        $this->assertCount(count($requests), $container, 'should perform all requests');
+
+        $this->assertCount(1, $response->devices());
+        $this->assertEquals($deviceRes, $response->devices()[0]->getObject());
     }
 
     public function testMockWithInvalidJson()
