5d7ab244ac50e3e9ddc4514e51d5b4b96f751656
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-04-05 03:05:19 +0200

    bump infection msi thresholds

--- tool/run_codestyle_tests.sh
+++ tool/run_codestyle_tests.sh
@@ -3,4 +3,4 @@ set -x
 cd $(git rev-parse --show-toplevel)
 if [ "$CODE_STYLE" = "true" ]; then make phpcs && echo "Code style OK"; fi
 if [ "$CODE_STYLE" = "true" ]; then vendor/bin/phpstan analyze -l 7 src && echo "Static analysis OK"; fi
-if [ "$CODE_STYLE" = "true" ]; then vendor/bin/infection && echo "Mutation analysis OK"; fi
+if [ "$CODE_STYLE" = "true" ]; then vendor/bin/infection --min-msi=80 --min-covered-msi=90 && echo "Mutation analysis OK"; fi
