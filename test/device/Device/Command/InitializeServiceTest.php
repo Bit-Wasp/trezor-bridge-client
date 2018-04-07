3ddbc4f000ab2af86e85d753cf6d78536fe48890
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-04-07 19:38:00 +0200

    exception is thrown when requesting an address from an uninitialized device

--- test/device/Device/Command/InitializeServiceTest.php
+++ test/device/Device/Command/InitializeServiceTest.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 namespace BitWasp\Test\Trezor\Device\Device\Command;
 
 use BitWasp\Test\Trezor\Device\Device\CommandTest;
+use BitWasp\Trezor\Device\Command\GetAddressService;
 use BitWasp\Trezor\Device\Command\InitializeService;
+use BitWasp\Trezor\Device\Exception\Failure\NotInitializedException;
+use BitWasp\Trezor\Device\RequestFactory;
+use BitWasp\Trezor\Device\UserInput\CurrentPinInput;
+use BitWasp\Trezor\Device\UserInput\DisabledUserInputRequest;
 use BitWasp\TrezorProto\Features;
 use BitWasp\TrezorProto\Initialize;
 
@@ -19,5 +24,20 @@ class InitializeServiceTest extends CommandTest
         $this->assertInstanceOf(Features::class, $features);
         $this->assertEquals("bitcointrezor.com", $features->getVendor());
         $this->assertFalse($features->getInitialized());
+    }
+
+    public function testUninitializedDevice()
+    {
+        $path = [44|0x80000000, 0|0x80000000, 0|0x80000000, 0, 0];
+
+        $reqFactory = new RequestFactory();
+        $getAddress = $reqFactory->getAddress('Bitcoin', $path, InputScriptType::SPENDADDRESS(), false);
+
+        $getAddressService = new GetAddressService();
+        $pinInput = new CurrentPinInput(new DisabledUserInputRequest());
+
+        $this->expectException(NotInitializedException::class);
+
+        $getAddressService->call($this->session, $pinInput, $getAddress);
     }
 }
